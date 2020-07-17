835a88b0e87fbf1e9d11f7eb8486c3653ace2206
thanuja94 <thanujag94@gmail.com>
2020-07-17 00:47:16 -0700

    creating Airbnb
    Creating app and set the background color

---
+++ Airbnb/Airbnb/Costants.swift
@@ -0,0 +1,13 @@
+//
+//  Costants.swift
+//  Airbnb
+//
+//  Created by Thanuj on 7/16/20.
+//  Copyright © 2020 Thanuj. All rights reserved.
+//
+
+import UIKit
+
+struct Colors {
+    static let cynaite = UIColor(red: 10/255, green: 195/255, blue: 227/255, alpha: 1)
+}
